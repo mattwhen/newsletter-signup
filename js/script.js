71a15884ff9ebde64e03c1c168b956781f42f078
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-06-09 13:32:01 -0500

    adds render feature after a valid email is submitted

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 /* FORM VALIDATION */
 const submitForm = document.getElementById('subscribe');
 const emailEl = document.getElementById('input-email');
+const emailElValue = document.getElementById('input-email').value;
 const errorMessage = document.getElementById('error-message');
+const panel = document.querySelector('.panel');
 
 submitForm.addEventListener('submit', validateForm);
 
@@ -21,12 +23,40 @@ function showError() {
     errorMessage.style.color = 'red';
     emailEl.classList.remove('success');
     emailEl.classList.add('error');
-
-
 }
 
 function showSuccess() {
-    emailEl.classList.remove('error');
+    emailEl.classList.remove('error');  
     emailEl.classList.add('success');
     errorMessage.style.display = 'none'
+
+    setTimeout(() => renderSuccessContent(), 1000);
+}
+
+function renderSuccessContent() {
+    // Removes existing content inside panel.
+    panel.textContent = '';
+    panel.classList.remove('panel');
+
+    // Create elements
+    let createCheckmark = document.createElement('img');
+    createCheckmark.src = '../assets/images/icon-success.svg'
+    let createTitle = document.createElement('h1');
+    let createP = document.createElement('p');
+    let createBtn = document.createElement('button');
+
+    // Modify class attributes
+    panel.setAttribute('style', 'height: 500px; width: 500px; background-color: white; border-radius: 30px; font-size: 16px; padding: 50px;');
+    createBtn.classList.add('dismissBtn');
+    createBtn.setAttribute('style', 'background-color: hsl(3, 18%, 26%), padding: 18px 10px; font-weight: 700px; width: 100%')
+    // Add text content 
+    createTitle.textContent = 'Thanks for subscribing!';
+    createP.textContent = `A confirmation email has been sent to ${emailEl.value}. Please open it and click the button inside to confirm your subscription.`;
+    createBtn.textContent = 'Dismiss message';
+
+    // Append all our elements in the panel
+    panel.appendChild(createCheckmark);
+    panel.appendChild(createTitle);
+    panel.appendChild(createP);
+    panel.appendChild(createBtn);
 }
