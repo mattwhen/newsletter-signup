291dfb385399c4790ecc17f27df540e3f9dba5d1
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-06-09 19:54:41 -0500

    commit finished the thanks for subscribing content and adding feature to reload the page when user clicks on the dismiss button

--- js/script.js
+++ js/script.js
@@ -8,55 +8,71 @@ const panel = document.querySelector('.panel');
 submitForm.addEventListener('submit', validateForm);
 
 function validateForm(e) {
-    e.preventDefault(); // Prevent default behavior when submitting form. 
-    // IF user clicks the button with no content in it, throw an error. 
-    return emailEl.value.trim() === '' ? showError() : showSuccess();
+	e.preventDefault(); // Prevent default behavior when submitting form.
+	// IF user clicks the button with no content in it, throw an error.
+	return emailEl.value.trim() === '' ? showError() : showSuccess();
 }
 
 const isEmailValid = (email) => {
-    const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regExp.test(email); // tests email validation and returns either 'True' or 'False'. 
-}
+	const regExp =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return regExp.test(email); // tests email validation and returns either 'True' or 'False'.
+};
 
 function showError() {
-    errorMessage.style.display = 'block';
-    errorMessage.style.color = 'red';
-    emailEl.classList.remove('success');
-    emailEl.classList.add('error');
+	errorMessage.style.display = 'block';
+	errorMessage.style.color = 'red';
+	emailEl.classList.remove('success');
+	emailEl.classList.add('error');
 }
 
 function showSuccess() {
-    emailEl.classList.remove('error');  
-    emailEl.classList.add('success');
-    errorMessage.style.display = 'none'
+	emailEl.classList.remove('error');
+	emailEl.classList.add('success');
+	errorMessage.style.display = 'none';
 
-    setTimeout(() => renderSuccessContent(), 1000);
+    // Render new content after a second, to give the user impression that it's contacting a server and doesn't render content immediately.
+	setTimeout(() => renderSuccessContent(), 1000);
 }
 
 function renderSuccessContent() {
-    // Removes existing content inside panel.
-    panel.textContent = '';
-    panel.classList.remove('panel');
+	// Removes existing content inside panel.
+	panel.textContent = '';
+	panel.classList.remove('panel');
 
-    // Create elements
-    let createCheckmark = document.createElement('img');
-    createCheckmark.src = '../assets/images/icon-success.svg'
-    let createTitle = document.createElement('h1');
-    let createP = document.createElement('p');
-    let createBtn = document.createElement('button');
+	// Create elements
+	let createCheckmark = document.createElement('img');
+	createCheckmark.src = '../assets/images/icon-success.svg';
+	let createTitle = document.createElement('h1');
+	let createP = document.createElement('p');
+	let createBtn = document.createElement('button');
 
-    // Modify class attributes
-    panel.setAttribute('style', 'height: 500px; width: 500px; background-color: white; border-radius: 30px; font-size: 16px; padding: 50px;');
-    createBtn.classList.add('dismissBtn');
-    createBtn.setAttribute('style', 'background-color: hsl(3, 18%, 26%), padding: 18px 10px; font-weight: 700px; width: 100%')
-    // Add text content 
-    createTitle.textContent = 'Thanks for subscribing!';
-    createP.textContent = `A confirmation email has been sent to ${emailEl.value}. Please open it and click the button inside to confirm your subscription.`;
-    createBtn.textContent = 'Dismiss message';
+	// Modify class attributes
+	panel.setAttribute(
+		'style',
+		'height: 500px; width: 500px; background-color: white; border-radius: 30px; font-size: 16px; padding: 50px;'
+	);
+	createBtn.classList.add('btn-primary');
+	createBtn.classList.add('btn');
+	createBtn.classList.add('submitBtn');
+	createTitle.classList.add('form-title');
 
-    // Append all our elements in the panel
-    panel.appendChild(createCheckmark);
-    panel.appendChild(createTitle);
-    panel.appendChild(createP);
-    panel.appendChild(createBtn);
+	// Add text content
+	createTitle.textContent = 'Thanks for subscribing!';
+	createP.textContent = `A confirmation email has been sent to ${emailEl.value}. Please open it and click the button inside to confirm your subscription.`;
+	createBtn.textContent = 'Dismiss message';
+
+	// Refresh page after dismissing the page
+	createBtn.addEventListener('click', () => {
+        // Reload the webpage after a second, to give the user impression that it's contacting a server and doesn't reload instantly.
+		setTimeout(() => {
+			location.reload();
+		}, 1000);
+	});
+
+	// Append all our elements in the panel
+	panel.appendChild(createCheckmark);
+	panel.appendChild(createTitle);
+	panel.appendChild(createP);
+	panel.appendChild(createBtn);
 }
